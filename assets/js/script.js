2ae45fcd1256e7d0d73ceb7c792d64d099d13ca3
Tom Bee <tburzynski2@yahoo.com>
2024-04-11 20:00:38 -0500

    saving mad libs story to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,31 @@ console.log("madlib");
 const madLibsApiUrl = "https://madlibs-api.vercel.app/api/random";
 const formEl = document.querySelector("#form-el");
 
-const submitButton =  document.getElementById('btn')
+const submitButton = document.getElementById("btn");
 
-formEl.addEventListener('submit', function(event) {
-  event.preventDefault(); 
-  
-  
+formEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+
   const formData = {};
   const inputs = formEl.querySelectorAll('input[type="text"]');
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     formData[input.id] = input.value;
   });
 
- 
   const formDataJSON = JSON.stringify(formData);
 
- 
-  localStorage.setItem('submittedData', formDataJSON);
+  localStorage.setItem("submittedData", formDataJSON);
 
-  
-  console.log('Form data submitted and stored in local storage.');
-  
-  
+  console.log("Form data submitted and stored in local storage.");
+
   formEl.reset();
 });
 
-  
 fetch(`https://octoproxymus.herokuapp.com?secret=walrus&url=${madLibsApiUrl}`)
-.then((response) => {
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
     return response.json();
   })
   .then((data) => {
@@ -84,13 +78,14 @@ fetch(`https://octoproxymus.herokuapp.com?secret=walrus&url=${madLibsApiUrl}`)
         // Append div to form
         formEl.appendChild(inputDivEl);
       }
+      // Save story to local storage
+      localStorage.setItem("storytext", JSON.stringify(apiResponse.text));
     }
   })
   .catch((error) => {
     // Handle errors here
     console.error("Error:", error);
   });
-
 
 function searchUnsplashImages(query, clientId, perPage) {
   const url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(
@@ -113,7 +108,4 @@ function searchUnsplashImages(query, clientId, perPage) {
     .then((data) => {
       return data.results;
     });
-  }
-  
-  
-
+}
